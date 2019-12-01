08800626b0af3e9eb49b53e04373fb468bc5fe22
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-01 19:28:36 +0200

    primitive user vertical added for further security implementation

---
+++ src/main/java/com/socialmedia/controllers/ResponseEntityProvider.java
@@ -0,0 +1,24 @@
+package com.socialmedia.controllers;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+public interface ResponseEntityProvider<T> {
+
+  default ResponseEntity<List<T>> provideResponseForList(List<T> list) {
+    if (list.isEmpty()) {
+      return new ResponseEntity<>(list, NOT_FOUND);
+    } else {
+      return ResponseEntity.ok(list);
+    }
+  }
+
+  default ResponseEntity<T> provideResponseForOptional(Optional<T> potOpt) {
+    return potOpt.map(ResponseEntity::ok)
+      .orElseGet(ResponseEntity.notFound()::build);
+  }
+}
--- src/test/java/com/socialmedia/controllers/UserControllerTest.java
+++ src/test/java/com/socialmedia/controllers/UserControllerTest.java
@@ -1,13 +1,20 @@
 package com.socialmedia.controllers;
 
+import com.socialmedia.models.User;
+import com.socialmedia.services.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,10 +26,15 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     public void starterTestForCoverAllIntegration() throws Exception{
+        User userDummy = new User();
+        when(userService.getCurrentUser()).thenReturn(Optional.of(userDummy));
 
-        mockMvc.perform(get("/api/v1/users"))
+        mockMvc.perform(get("/api/v1/users/current"))
                 .andExpect(status().isOk());
     }
 }
