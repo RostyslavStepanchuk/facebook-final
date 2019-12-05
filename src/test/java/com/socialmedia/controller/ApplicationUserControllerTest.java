0c81497990c8529a5d2c1661166148e3e1cb0ced
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-05 23:48:07 +0200

    principal mock removed from ApplicationUserController test

--- src/test/java/com/socialmedia/controller/ApplicationUserControllerTest.java
+++ src/test/java/com/socialmedia/controller/ApplicationUserControllerTest.java
@@ -8,15 +8,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
-import java.security.Principal;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -32,9 +28,6 @@ public class ApplicationUserControllerTest {
 
     @Autowired
     private ObjectMapper mapper;
-
-    @MockBean
-    private Principal principal;
 
     @Test
     public void getCurrentUserShouldBlockRequestWithoutAuthentication() throws Exception{
@@ -58,12 +51,9 @@ public class ApplicationUserControllerTest {
             .openAccount(true)
             .build();
 
-        when(principal.getName()).thenReturn(applicationUser.getUsername());
-
         String result = mapper.writeValueAsString(applicationUser);
 
-        RequestBuilder requestBuilder = get("/api/v1/users/current")
-            .principal(principal);
+        RequestBuilder requestBuilder = get("/api/v1/users/current");
 
         mockMvc.perform(requestBuilder)
             .andExpect(status().isOk());
