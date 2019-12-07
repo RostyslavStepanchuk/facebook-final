da781a861185fb6ecec603a3983fffda19f62383
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-07 12:51:34 +0200

    Exception handler implemented. Tests updated

--- src/main/java/com/socialmedia/controller/ExceptionController.java
+++ src/main/java/com/socialmedia/controller/ExceptionController.java
@@ -1,4 +1,16 @@
 package com.socialmedia.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+@ControllerAdvice
 public class ExceptionController {
+
+  @ExceptionHandler(Throwable.class)
+  public ResponseEntity<String> globalHandler(Exception e) {
+    return new ResponseEntity<>(e.getMessage(), CONFLICT);
+  }
 }
--- src/test/java/com/socialmedia/controller/util/TestConstants.java
+++ src/test/java/com/socialmedia/controller/util/TestConstants.java
@@ -1,4 +1,23 @@
 package com.socialmedia.controller.util;
 
 public class TestConstants {
+  public static final String USER_USERNAME = "testUser";
+  public static final String USER_PASSWORD = "passw1234";
+  public static final String USER_EMAIL = "test@test.com";
+  public static final String USER_FIRST_NAME = "Tyler";
+  public static final String USER_LAST_NAME = "Durden";
+  public static final Long USER_BIRTH_DATE = 659998800000L;
+  public static final String USER_AVATAR_URL = "http://avataurl.com";
+  public static final String USER_REFRESH_TOKEN = "034daREFRESHTOKENf341fd";
+  public static final String USER_FORGOT_PASSWORD_TOKEN = "41fdFORGOTPASSWORDTOKEN093wgs";
+  public static final Boolean USER_OPEN_ACCOUNT = true;
+  
+  public static final String URL_GET_CURRENT_USER = "/api/v1/users/current";
+  public static final String URL_SIGN_UP = "/api/v1/users/sign-up";
+  public static final String URL_ACCESS_TOKEN = "/api/v1/auth/access-token";
+  public static final String URL_GET_ALL_CHATS = "/api/v1/chats";
+
+  public static final String CONTENT_TYPE_JSON = "application/json";
+  
+  
 }
