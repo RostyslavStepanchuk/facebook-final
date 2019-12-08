6e464f7cf19f352105ee177c7172f86e271ae550
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-08 19:17:11 +0200

    user endpoints draft created

--- src/main/java/com/socialmedia/controller/UserController.java
+++ src/main/java/com/socialmedia/controller/UserController.java
@@ -2,6 +2,7 @@ package com.socialmedia.controller;
 
 import com.socialmedia.dto.security.Token;
 import com.socialmedia.dto.security.UserCredentials;
+import com.socialmedia.dto.user.UserDtoIn;
 import com.socialmedia.dto.user.UserDtoOut;
 import com.socialmedia.mapper.UserMapper;
 import com.socialmedia.model.ApplicationUser;
@@ -9,12 +10,17 @@ import com.socialmedia.service.AuthenticationService;
 import com.socialmedia.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.InvocationTargetException;
 import java.security.Principal;
 import java.util.Optional;
 
@@ -42,7 +48,7 @@ public class UserController implements ResponseEntityProvider {
     return provideResponseForOptional(user.map(userMapper::toFullDto));
   }
 
-  @PostMapping("/sign-up")
+  @PostMapping
   public ResponseEntity<Token> signUp(@RequestBody UserCredentials user) {
 
     userService.addUser(userMapper.toEntity(user));
@@ -51,6 +57,31 @@ public class UserController implements ResponseEntityProvider {
         .map(Token::new)
         .orElseThrow(() -> new RuntimeException("Unable to get access token for newly created user"));
     return ResponseEntity.ok(token);
+  }
+
+  @PutMapping("/{username}")
+  public ResponseEntity<UserDtoOut> updateUser(Principal principal,
+                                               @PathVariable String username,
+                                               @RequestBody UserDtoIn user)
+      throws InvocationTargetException, IllegalAccessException {
+
+    if (!principal.getName().equals(username)) {
+      throw new BadCredentialsException(String.format("User %s can't be updated from this account", username));
+    }
+
+    Optional<ApplicationUser> applicationUser = userService.updateUser(username, userMapper.toEntity(user));
+
+    return provideResponseForOptional(applicationUser.map(userMapper::toFullDto));
+  }
+
+  @DeleteMapping("/{username}")
+  public ResponseEntity<UserDtoOut> deleteUser(@PathVariable String username, Principal principal) {
+
+    if (!principal.getName().equals(username)) {
+      throw new BadCredentialsException(String.format("User %s can't be deleted from this account", username));
+    }
+    Optional<ApplicationUser> deletedUser = userService.delete(username);
+    return provideResponseForOptional(deletedUser.map(userMapper::toFullDto));
   }
 
 }
--- src/main/java/com/socialmedia/service/UserServiceImpl.java
+++ src/main/java/com/socialmedia/service/UserServiceImpl.java
@@ -2,10 +2,12 @@ package com.socialmedia.service;
 
 import com.socialmedia.model.ApplicationUser;
 import com.socialmedia.repository.UserRepository;
+import com.socialmedia.util.SmartCopyBeanUtilsBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 
 @Service
@@ -13,16 +15,43 @@ public class UserServiceImpl implements UserService {
 
   private UserRepository userRepository;
   private BCryptPasswordEncoder bcryptPasswordEncoder;
+  private SmartCopyBeanUtilsBean smartCopyBeanUtilsBean;
 
   @Autowired
-  public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bcryptPasswordEncoder) {
+  public UserServiceImpl(UserRepository userRepository,
+                         BCryptPasswordEncoder bcryptPasswordEncoder,
+                         SmartCopyBeanUtilsBean smartCopyBeanUtilsBean) {
     this.userRepository = userRepository;
     this.bcryptPasswordEncoder = bcryptPasswordEncoder;
+    this.smartCopyBeanUtilsBean = smartCopyBeanUtilsBean;
   }
 
   @Override
   public Optional<ApplicationUser> getUser(String username) {
     return userRepository.findById(username);
+  }
+
+
+  @Override
+  public Optional<ApplicationUser> delete(String username) {
+
+    Optional<ApplicationUser> user = userRepository.findById(username);
+    user.ifPresent(userRepository::delete);
+    return user;
+  }
+
+  @Override
+  public Optional<ApplicationUser> updateUser(String username,
+                                              ApplicationUser incomingEntity)
+      throws InvocationTargetException, IllegalAccessException {
+
+    Optional<ApplicationUser> existingEntity = userRepository.findById(username);
+    if (existingEntity.isPresent()) {
+      ApplicationUser user = existingEntity.get();
+      smartCopyBeanUtilsBean.copyProperties(user, incomingEntity);
+      userRepository.save(user);
+    }
+    return existingEntity;
   }
 
   @Override
