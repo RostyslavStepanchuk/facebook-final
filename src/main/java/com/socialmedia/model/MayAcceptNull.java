aa4eab86d04ea10a86f301a8f532554ec2ea483e
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-08 19:16:43 +0200

    tests refactored

---
+++ src/main/java/com/socialmedia/model/MayAcceptNull.java
@@ -0,0 +1,13 @@
+package com.socialmedia.model;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = FieldsValueMatchValidator.class)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DoNotCopyNull {
+}
