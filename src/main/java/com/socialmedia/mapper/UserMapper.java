13a276ac6366cd96aa5c3ed4409d23403e4a430b
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-13 21:34:52 +0200

    extended user registration data and emailConfirmed field for user entity implemented

--- src/main/java/com/socialmedia/mapper/UserMapper.java
+++ src/main/java/com/socialmedia/mapper/UserMapper.java
@@ -1,9 +1,9 @@
 package com.socialmedia.mapper;
 
-import com.socialmedia.dto.security.UserCredentials;
 import com.socialmedia.dto.user.UserDtoIn;
 import com.socialmedia.dto.user.UserDtoOut;
 import com.socialmedia.dto.user.UserLabelDtoOut;
+import com.socialmedia.dto.user.UserRegistrationDtoIn;
 import com.socialmedia.model.ApplicationUser;
 import com.socialmedia.service.UserService;
 import org.modelmapper.ModelMapper;
@@ -12,14 +12,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public final class UserMapper extends
-    AbstractControllerToServiceMapper<ApplicationUser, String, UserDtoIn, UserDtoOut, UserService> {
+    AbstractControllerToCrudServiceMapper<ApplicationUser, String, UserDtoIn, UserDtoOut, UserService> {
 
   @Autowired
   public UserMapper(ModelMapper modelMapper, UserService crudService) {
     super(modelMapper, crudService);
   }
 
-  public void createFromCredentials(UserCredentials credentials) {
+  public void create(UserRegistrationDtoIn credentials) {
     ApplicationUser entity = entityOf(credentials);
     crudService.create(entity);
   }
@@ -35,8 +35,8 @@ public final class UserMapper extends
     return modelMapper.map(dtoIn, ApplicationUser.class);
   }
 
-  private ApplicationUser entityOf(UserCredentials credentials) {
-    return modelMapper.map(credentials, ApplicationUser.class);
+  private ApplicationUser entityOf(UserRegistrationDtoIn userData) {
+    return modelMapper.map(userData, ApplicationUser.class);
   }
 
   private UserLabelDtoOut userLabelDtoOf(ApplicationUser entity) {
