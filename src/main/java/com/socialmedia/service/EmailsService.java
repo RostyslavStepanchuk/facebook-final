e1b1801cb5059a581549e1bcc669e90a52a2e3e3
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-15 19:52:00 +0200

    Field injection removed from EmailHandler, uuid generation implemented for EmailAddress

--- src/main/java/com/socialmedia/service/EmailsService.java
+++ src/main/java/com/socialmedia/service/EmailsService.java
@@ -5,11 +5,20 @@ import com.socialmedia.repository.EmailRepository;
 import com.socialmedia.util.SmartCopyBeanUtilsBean;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class EmailsService extends AbstractCrudService<EmailAddress, String, EmailRepository> {
 
   public EmailsService(EmailRepository jpaRepository,
                        SmartCopyBeanUtilsBean beanUtilsBean) {
     super(jpaRepository, beanUtilsBean);
+  }
+
+  @Override
+  public EmailAddress create(EmailAddress entity) {
+    entity.setConfirmationId(UUID.randomUUID().toString());
+    entity.setIsConfirmed(false);
+    return super.create(entity);
   }
 }
