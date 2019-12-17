550f6c1f36bf80fed0194794b2bbcb7a416b8b1c
MaksymSimonov <you@example.com>
2019-12-17 11:19:25 +0200

    latest modifications for s3

--- src/main/java/com/socialmedia/service/AmazonService.java
+++ src/main/java/com/socialmedia/service/AmazonService.java
@@ -1,23 +1,15 @@
 package com.socialmedia.service;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.PropertiesFileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
-import javax.xml.ws.soap.Addressing;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -25,7 +17,6 @@ import java.util.Date;
 
 @Service
 @Configuration
-//@PropertySource("classpath:/amazonKeys/keys-for-s3.properties")
 public class AmazonService {
 
   @Autowired
@@ -35,17 +26,6 @@ public class AmazonService {
   private String endpointUrl;
   @Value("${amazonProperties.bucketName}")
   private String bucketName;
-  //@Value("${secretKey.accessKey}")
-  //private String accessKey;
-  //@Value("${secretKey.secretKey}")
-  //private String secretKey;
-
-
-  //@PostConstruct
-  //private void initializeAmazon() {
-  //  AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-  //  this.s3client = new AmazonS3Client(credentials);
-  //}
 
   public String uploadFile(MultipartFile multipartFile) {
     String fileUrl = "";
