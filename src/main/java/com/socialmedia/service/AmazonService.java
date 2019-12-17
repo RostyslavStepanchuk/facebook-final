e05ddd64d59400ef518bc4d0544f710741d8eb62
MaksymSimonov <simonovmax90@gmail.com>
2019-12-17 08:24:24 +0200

    fixed S3 for Travis

--- src/main/java/com/socialmedia/service/AmazonService.java
+++ src/main/java/com/socialmedia/service/AmazonService.java
@@ -2,18 +2,22 @@ package com.socialmedia.service;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.PropertiesFileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.xml.ws.soap.Addressing;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,25 +25,33 @@ import java.util.Date;
 
 @Service
 @Configuration
-@PropertySource("classpath:/amazonKeys/keys-for-s3.properties")
+//@PropertySource("classpath:/amazonKeys/keys-for-s3.properties")
 public class AmazonService {
 
+  @Autowired
   private AmazonS3 s3client;
 
   @Value("${amazonProperties.endpointUrl}")
   private String endpointUrl;
   @Value("${amazonProperties.bucketName}")
   private String bucketName;
-  @Value("${secretKey.accessKey}")
-  private String accessKey;
-  @Value("${secretKey.secretKey}")
-  private String secretKey;
+  //@Value("${secretKey.accessKey}")
+  //private String accessKey;
+  //@Value("${secretKey.secretKey}")
+  //private String secretKey;
 
-  @PostConstruct
-  private void initializeAmazon() {
-    AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-    this.s3client = new AmazonS3Client(credentials);
+  @Bean
+  public AmazonS3 amazonS3Client() {
+    AWSCredentials credentials = new PropertiesFileCredentialsProvider("src/main/resources/awsKeys/keys-for-s3.properties")
+            .getCredentials();
+    return new AmazonS3Client(credentials);
   }
+
+  //@PostConstruct
+  //private void initializeAmazon() {
+  //  AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+  //  this.s3client = new AmazonS3Client(credentials);
+  //}
 
   public String uploadFile(MultipartFile multipartFile) {
     String fileUrl = "";
