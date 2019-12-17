1ffe6f438423267af1d9b7cf8128c8e940161693
Stanislav Kosinskiy <stanislav.kosinski@gmail.com>
2019-12-17 20:19:07 +0200

    fixed merge issue

--- src/main/java/com/socialmedia/mapper/PostMapper.java
+++ src/main/java/com/socialmedia/mapper/PostMapper.java
@@ -3,25 +3,26 @@ package com.socialmedia.mapper;
 import com.socialmedia.dto.post.PostDtoIn;
 import com.socialmedia.dto.post.PostDtoOut;
 import com.socialmedia.model.Post;
+import com.socialmedia.service.PostService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PostMapper {
-
-  private ModelMapper modelMapper;
+public class PostMapper extends AbstractControllerToCrudServiceMapper<Post,Long, PostDtoIn, PostDtoOut, PostService> {
 
   @Autowired
-  public PostMapper(ModelMapper modelMapper) {
-    this.modelMapper = modelMapper;
+  public PostMapper(ModelMapper modelMapper, PostService postService) {
+    super(modelMapper, postService);
   }
 
-  public PostDtoOut toDto(Post entity) {
+  @Override
+  PostDtoOut responseDtoOf(Post entity) {
     return modelMapper.map(entity, PostDtoOut.class);
   }
 
-  public Post toEntity(PostDtoIn postDtoIn) {
-    return modelMapper.map(postDtoIn, Post.class);
+  @Override
+  Post entityOf(PostDtoIn dtoIn) {
+    return modelMapper.map(dtoIn, Post.class);
   }
 }
