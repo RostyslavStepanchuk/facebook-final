5ecbce15d61c3d99cc29e1dd539600f6b24bd771
Rostyslav Stepanchuk <ros_st@ukr.net>
2019-12-26 18:20:38 +0200

    console.log and unused imports removed

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import { Container, Grid } from '@material-ui/core'
 import CreatePost from '../../components/CreatePost/CreatePost'
 
-import { withStyles } from "@material-ui/styles";
+import { withStyles } from '@material-ui/styles'
 import styles from './HomePageStyles'
 
 class HomePage extends Component {
