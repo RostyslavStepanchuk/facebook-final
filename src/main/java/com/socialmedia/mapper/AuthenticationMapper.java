0fcc2e5d8324c37a7d78f30a022be81854f94eee
Rostyslav Stepanchuk <ros_st@ukr.net>
2020-01-04 22:56:15 +0200

    minor CI changes

--- src/main/java/com/socialmedia/mapper/AuthenticationMapper.java
+++ src/main/java/com/socialmedia/mapper/AuthenticationMapper.java
@@ -2,22 +2,32 @@ package com.socialmedia.mapper;
 
 import com.socialmedia.dto.security.Token;
 import com.socialmedia.dto.security.UserCredentials;
+import com.socialmedia.model.ApplicationUser;
 import com.socialmedia.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class AuthenticationMapper {
 
   private AuthenticationService authenticationService;
+  private UserMapper userMapper;
 
   @Autowired
-  public AuthenticationMapper(AuthenticationService authenticationService) {
+  public AuthenticationMapper(AuthenticationService authenticationService, UserMapper userMapper) {
     this.authenticationService = authenticationService;
+    this.userMapper = userMapper;
   }
 
   public Token getAccessToken(UserCredentials credentials) {
 
     return authenticationService.getAccessToken(credentials);
+  }
+
+  public Token refreshTokens(String refreshToken, String username, HttpServletResponse resp) {
+    ApplicationUser applicationUser = userMapper.entityOf(username);
+    return authenticationService.refreshTokens(refreshToken, applicationUser, resp);
   }
 }
--- src/main/java/com/socialmedia/service/AuthenticationService.java
+++ src/main/java/com/socialmedia/service/AuthenticationService.java
@@ -2,10 +2,12 @@ package com.socialmedia.service;
 
 import com.socialmedia.dto.security.Token;
 import com.socialmedia.dto.security.UserCredentials;
+import com.socialmedia.model.ApplicationUser;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Service
 public interface AuthenticationService {
@@ -15,4 +17,6 @@ public interface AuthenticationService {
   Token getAccessToken(String username, String password);
 
   UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req);
+
+  Token refreshTokens(String refreshToken, ApplicationUser user, HttpServletResponse resp);
 }
