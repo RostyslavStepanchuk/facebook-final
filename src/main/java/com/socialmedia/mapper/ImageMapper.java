e81c4c56fb6c521274110ccd68e5ab8e99de94b4
MaksymSimonov <you@example.com>
2020-01-05 22:35:52 +0200

    entity for image

---
+++ src/main/java/com/socialmedia/mapper/ImageMapper.java
@@ -0,0 +1,38 @@
+package com.socialmedia.mapper;
+
+import com.socialmedia.dto.image.ImageDtoIn;
+import com.socialmedia.dto.image.ImageDtoOut;
+import com.socialmedia.model.Image;
+import com.socialmedia.service.AmazonService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class ImageMapper extends AbstractControllerToCrudServiceMapper<Image, Long, ImageDtoIn, ImageDtoOut, AmazonService> {
+
+    @Autowired
+    public ImageMapper(ModelMapper modelMapper,
+                      AmazonService crudService) {
+        super(modelMapper, crudService);
+    }
+
+    public ImageDtoOut responseDtoOf(Image entity) {
+        return modelMapper.map(entity, ImageDtoOut.class);
+    }
+
+    @Override
+    public Image entityOf(ImageDtoIn dtoIn) {
+        return modelMapper.map(dtoIn, Image.class);
+    }
+
+    public ImageDtoOut uploadFile(MultipartFile file) {
+        return responseDtoOf(crudService.uploadFile(file));
+    }
+
+    public Boolean deleteFile(String fileName) {
+        return crudService.deleteFile(fileName);
+    }
+}
--- src/main/java/com/socialmedia/service/AmazonService.java
+++ src/main/java/com/socialmedia/service/AmazonService.java
@@ -4,10 +4,14 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.socialmedia.model.Image;
+import com.socialmedia.repository.ImageRepository;
+import com.socialmedia.util.SmartCopyBeanUtilsBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,16 +19,20 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 @Configuration
 @Slf4j
-public class AmazonService {
+public class AmazonService extends AbstractCrudService<Image, Long, ImageRepository>{
 
   private AmazonS3 s3client;
 
   @Autowired
-  public AmazonService(AmazonS3 s3client) {
+  public AmazonService(ImageRepository jpaRepository,
+                       SmartCopyBeanUtilsBean beanUtilBean,
+                       AmazonS3 s3client) {
+    super(jpaRepository, beanUtilBean);
     this.s3client = s3client;
   }
 
@@ -33,16 +41,45 @@ public class AmazonService {
   @Value("${amazonProperties.bucketName}")
   private String bucketName;
 
-  public String uploadFile(MultipartFile multipartFile) {
+  public Image uploadFile(MultipartFile multipartFile) {
     try {
       File file = convertMultiPartToFile(multipartFile);
       String fileName = generateFileName(multipartFile);
       uploadFileToS3bucket(fileName, file);
-      return endpointUrl + "/" + bucketName + "/" + fileName;
+      Image image = new Image();
+      image.setKey(fileName);
+      image.setSrc(endpointUrl + "/" + bucketName + "/" + fileName);
+      jpaRepository.save(image);
+      return image;
     } catch (Exception exc) {
       log.error(exc.getMessage(), exc);
       throw new RuntimeException(exc);
     }
+  }
+//  public String uploadFile(MultipartFile multipartFile) {
+//    try {
+//      File file = convertMultiPartToFile(multipartFile);
+//      String fileName = generateFileName(multipartFile);
+//      uploadFileToS3bucket(fileName, file);
+//      return endpointUrl + "/" + bucketName + "/" + fileName;
+//    } catch (Exception exc) {
+//      log.error(exc.getMessage(), exc);
+//      throw new RuntimeException(exc);
+//    }
+//  }
+
+  public Boolean deleteFile(String fileName) {
+    boolean deleted;
+    if(deleteFileFromS3Bucket(fileName)){
+      Optional<Image> existingEntity = jpaRepository.findByKey(fileName);
+      existingEntity.ifPresent(image -> {
+        jpaRepository.delete(image);
+      });
+      deleted = true;
+    } else {
+      deleted = false;
+    }
+    return deleted;
   }
 
   private File convertMultiPartToFile(MultipartFile file) throws IOException {
@@ -62,8 +99,10 @@ public class AmazonService {
         .withCannedAcl(CannedAccessControlList.PublicRead));
   }
 
-  public boolean deleteFileFromS3Bucket(String fileName) {
+  private Boolean deleteFileFromS3Bucket(String fileName) {
     s3client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
     return !s3client.doesObjectExist(bucketName, fileName);
   }
+
+
 }
