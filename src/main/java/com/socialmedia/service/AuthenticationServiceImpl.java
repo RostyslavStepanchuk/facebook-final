c4fb58b4cb75ff4deb1a31b7ef2b4d2671309925
Rostyslav Stepanchuk <ros_st@ukr.net>
2020-01-08 16:58:36 +0200

    refreshToken implemented on backend

--- src/main/java/com/socialmedia/service/AuthenticationServiceImpl.java
+++ src/main/java/com/socialmedia/service/AuthenticationServiceImpl.java
@@ -4,11 +4,13 @@ package com.socialmedia.service;
 import com.socialmedia.dto.security.Token;
 import com.socialmedia.dto.security.UserCredentials;
 import com.socialmedia.model.ApplicationUser;
+import com.socialmedia.model.TokensData;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,12 +18,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.UUID;
 
+import static com.socialmedia.security.SecurityConstants.ACCESS_TOKEN_MAX_AGE;
 import static com.socialmedia.security.SecurityConstants.HEADER_STRING;
+import static com.socialmedia.security.SecurityConstants.REFRESH_TOKEN_MAX_AGE;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -30,14 +34,17 @@ public class AuthenticationServiceImpl implements AuthenticationService {
   public String secret;
 
   private AuthenticationManager authenticationManager;
+  private UserService userService;
 
   @Autowired
-  public AuthenticationServiceImpl(AuthenticationManager authenticationManager) {
+  public AuthenticationServiceImpl(AuthenticationManager authenticationManager, @Lazy UserService userService) {
     this.authenticationManager = authenticationManager;
+    this.userService = userService;
   }
 
   @Override
   public Token getAccessToken(UserCredentials credentials) {
+
     return getAccessToken(credentials.getUsername(), credentials.getPassword());
   }
 
@@ -59,6 +66,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
   }
 
   public UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+
     String token = req.getHeader(HEADER_STRING);
     UsernamePasswordAuthenticationToken result = null;
     if (token != null) {
@@ -72,22 +80,37 @@ public class AuthenticationServiceImpl implements AuthenticationService {
   }
 
   @Override
-  public Token refreshTokens(String refreshToken, ApplicationUser user, HttpServletResponse resp) {
+  public Token getAccessTokenByRefreshToken(String refreshToken, String username) {
+
+    ApplicationUser user = userService.getById(username);
     String userRefreshToken = user.getTokensData().getRefreshToken();
     Date tokenExpiration = new Date(user.getTokensData().getRefreshTokenValidTill());
 
     if (userRefreshToken.equals(refreshToken) &
-        tokenExpiration.before(new Date())) {
+        tokenExpiration.after(new Date())) {
       return generateAccessToken(user.getUsername());
     }
 
-    throw new BadCredentialsException("Your tokens have expired, please log in");
+    throw new BadCredentialsException("Access to application have expired, please log in");
+  }
+
+  @Override
+  public String generateRefreshToken(String username) {
+
+    ApplicationUser user = userService.getById(username);
+    String refreshToken = UUID.randomUUID().toString();
+    TokensData tokensData = user.getTokensData();
+    tokensData.setRefreshToken(refreshToken);
+    tokensData.setRefreshTokenValidTill(System.currentTimeMillis() + REFRESH_TOKEN_MAX_AGE);
+    userService.update(user.getUsername(), user);
+    return refreshToken;
   }
 
   private Token generateAccessToken(String subject) {
+
     String token = Jwts.builder()
         .setIssuedAt(new Date())
-        .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 15)))
+        .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_MAX_AGE))
         .setSubject(subject)
         .addClaims(Collections.emptyMap())
         .signWith(SignatureAlgorithm.HS512, secret)
---
+++ src/main/java/com/socialmedia/util/CookieMgr.java
@@ -0,0 +1,24 @@
+package com.socialmedia.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import static com.socialmedia.security.SecurityConstants.REFRESH_TOKEN_COOKIE_NAME;
+import static com.socialmedia.security.SecurityConstants.REFRESH_TOKEN_COOKIE_PATH;
+import static com.socialmedia.security.SecurityConstants.REFRESH_TOKEN_MAX_AGE;
+
+@Component
+public class CookieMgr {
+
+  public void addRefreshTokenCookie(HttpServletResponse resp, String refreshToken) {
+    Cookie cookie = new Cookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken);
+    cookie.setPath(REFRESH_TOKEN_COOKIE_PATH);
+    cookie.setMaxAge((int)(REFRESH_TOKEN_MAX_AGE/1000));
+//    cookie.setSecure(true);
+    cookie.setHttpOnly(true);
+    resp.addCookie(cookie);
+  }
+
+}
