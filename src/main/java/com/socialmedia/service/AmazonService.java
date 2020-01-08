df72130986807acb673947098b9b4cbe51a17f1a
MaksymSimonov <you@example.com>
2020-01-08 18:34:33 +0200

    travis

--- src/main/java/com/socialmedia/service/AmazonService.java
+++ src/main/java/com/socialmedia/service/AmazonService.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
