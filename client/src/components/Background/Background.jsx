a63111b7e00576119d61bf670cb0907698d24855
Rostyslav Stepanchuk <ros_st@ukr.net>
2020-01-17 20:37:25 +0200

    children specified in propTypes

--- client/src/components/Background/Background.jsx
+++ client/src/components/Background/Background.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { BACKGROUND_COLOR } from '../../utils/constants/styleConstants'
 
-const Background = (props) => {
+const Background = ({ children }) => {
   return (
     <div style={{
       backgroundColor: BACKGROUND_COLOR,
       minHeight: 'calc(100vh - 100px)'
     }}>
-      {props.children}
+      {children}
     </div>
   )
+}
+
+Background.propTypes = {
+  children: PropTypes.array
 }
 
 export default Background
