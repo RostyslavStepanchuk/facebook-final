8f98ce0b4971f69597f9be742f0cc0fb13cd3024
MaksymSimonov <simonovmax90@gmail.com>
2020-01-18 18:50:16 +0200

    added friend list

---
+++ client/src/components/FriendRequestsList/friendRequestsListStyles.js
@@ -0,0 +1,51 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  container: {
+    textAlign: 'left'
+  },
+  header: {
+    color: '#1c1e21',
+    padding: '8px 10px',
+    fontWeight: 900,
+    borderBottom: '1px solid #dddfe2',
+    paddingRight: 24,
+    backgroundColor: '#f5f6f7'
+  },
+  count: {
+    color: '#90949c',
+    fontWeight: 500
+  },
+  gridContainer: {
+    padding: '8px 10px',
+  },
+  gridItem: {
+    display: 'flex',
+    border: '1px solid #e9ebee',
+    marginBottom: 8,
+  },
+  friendAvatar: {
+    width: '30%',
+  },
+  friendInfo: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '70%',
+    padding: '0 10px'
+
+  },
+  image: {
+    width: '100%',
+  },
+  userName: {
+    margin: 0,
+    wordBreak: 'break-word'
+  },
+  confirmBtn: {
+    color: '#8bc34a'
+  }
+
+}))
+
+export default useStyles
--- client/src/components/FriendsList/FriendsList.jsx
+++ client/src/components/FriendsList/FriendsList.jsx
@@ -1,11 +1,102 @@
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
+import {
+  Button, Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  Slide,
+  Typography
+} from '@material-ui/core'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import useStyles from './friendsListStyles'
+import HighlightOffIcon from '@material-ui/icons/HighlightOff'
+import MailOutlineIcon from '@material-ui/icons/MailOutline'
+import Tooltip from '@material-ui/core/Tooltip'
+import IconButton from '@material-ui/core/IconButton'
 
-const FriendsList = () => {
+const Transition = React.forwardRef(function Transition (props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+
+const FriendsList = ({ user }) => {
+  const classes = useStyles()
+  const [openDialog, setOpenDialog] = useState(false)
+  const { friends } = user
+
+  const handleModal = () => {
+    setOpenDialog(!openDialog)
+  }
+
+  const handleModalDelete = () => {
+    handleModal()
+    //deleteFriend(postId)
+  }
+
+  const friendsList = friends.map((friend) =>
+    <Grid item xs={12} sm={6} className={classes.gridItem}>
+      <div className={classes.friendAvatar}>
+        <img src={friend.avatar.src} className={classes.image} alt='Avatar'/>
+      </div>
+      <div className={classes.friendInfo}>
+        <p className={classes.userName}>{friend.firstName} {friend.lastName}</p>
+        <div>
+          <Tooltip title="Send message">
+            <IconButton color="primary" aria-label="Send message">
+              <MailOutlineIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Remove friend">
+            <IconButton color="secondary" onClick={handleModal} aria-label="Remove friend">
+              <HighlightOffIcon />
+            </IconButton>
+          </Tooltip>
+          <Dialog
+            open={openDialog}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleModal}
+          >
+            <DialogTitle id='alert'>Delete friend</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Are you sure you want to delete friend?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button variant='contained' color='primary' onClick={handleModal}>
+                Cancel
+              </Button>
+              <Button variant='contained' color='secondary' onClick={handleModalDelete}>
+                Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      </div>
+    </Grid>
+  )
+
   return (
-    <Fragment>
-      FriendsList
-    </Fragment>
+    <div className={classes.container}>
+      <Typography className={classes.header}  variant='subtitle1' component='div'>
+        Friends <span className={classes.count}>{friends.length}</span>
+      </Typography>
+      <Grid className={classes.gridContainer} container>
+        {friendsList}
+      </Grid>
+    </div>
   )
 }
 
-export default FriendsList
+FriendsList.propTypes = {
+  user: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  user: state.auth.user,
+})
+
+export default connect(mapStateToProps, null)(FriendsList)
