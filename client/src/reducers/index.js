7c6ff0dc981ffa1ef2bae39b01bad3f4e51e2987
Taras Bashuk <bashuk.taras@gmail.com>
2020-01-21 17:17:58 +0200

    Implemented search

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 import auth from './auth'
 import posts from './posts'
+import search from './search'
 
 export default combineReducers({
   auth,
   posts,
+  search,
   toastr: toastrReducer
 
 })
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -13,7 +13,7 @@ const initialState = {
   loading: false
 }
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   const { type, payload } = action
 
   switch (type) {
