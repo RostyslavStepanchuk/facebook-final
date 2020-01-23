93d1ba97358faa284d3a855e44b0cdea1d6f4292
MaksymSimonov <simonovmax90@gmail.com>
2020-01-23 02:27:43 +0200

    created getChats

--- src/main/java/com/socialmedia/repository/ChatRepository.java
+++ src/main/java/com/socialmedia/repository/ChatRepository.java
@@ -1,7 +1,11 @@
 package com.socialmedia.repository;
 
+import com.socialmedia.model.ApplicationUser;
 import com.socialmedia.model.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    List<Chat> getAllByParticipantsContaining(ApplicationUser user);
 }
