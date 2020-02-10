de03791dffa5f8eb0974d4b691a0b8c44a859b27
MaksymSimonov <simonovmax90@gmail.com>
2020-02-10 15:40:07 +0200

    graph for search

--- src/main/java/com/socialmedia/util/friendshipSuggestions/Vertex.java
+++ src/main/java/com/socialmedia/util/friendshipSuggestions/Vertex.java
@@ -1,4 +1,26 @@
 package com.socialmedia.util.friendshipSuggestions;
 
-public class Vertex {
+import com.socialmedia.model.ApplicationUser;
+
+import java.util.Objects;
+
+class Vertex {
+    ApplicationUser user;
+
+    Vertex(ApplicationUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(user, vertex.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user);
+    }
 }
