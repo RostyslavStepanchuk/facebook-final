dc22e8344afe280c1a925440dcbd93b7a13e0247
MaksymSimonov <simonovmax90@gmail.com>
2020-02-11 03:39:36 +0200

    fixed graph

---
+++ src/main/java/com/socialmedia/util/friendship/FriendshipSuggestions.java
@@ -0,0 +1,11 @@
+package com.socialmedia.util.friendship;
+
+import com.socialmedia.model.ApplicationUser;
+
+public class FriendshipSuggestions {
+  private ApplicationUser user;
+
+  public FriendshipSuggestions(ApplicationUser user) {
+    this.user = user;
+  }
+}
