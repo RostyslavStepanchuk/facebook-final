44d76281d99a3faabc3b7ea0cccd1938f2f54169
Rostyslav Stepanchuk <ros_st@ukr.net>
2020-02-18 15:38:22 +0200

    frame Options disabled

--- src/main/java/com/socialmedia/security/WebSecurity.java
+++ src/main/java/com/socialmedia/security/WebSecurity.java
@@ -47,7 +47,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable().authorizeRequests()
+    http.headers()
+        .frameOptions()
+        .disable()
+        .and()
+        .cors().and().csrf().disable().authorizeRequests()
         .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
         .antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
         .antMatchers(HttpMethod.POST, USE_REFRESH_TOKEN_URL).permitAll()
